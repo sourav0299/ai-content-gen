1aca4c6b4e563c8dd319ef07ad94384091839764
sourav0299 <105085605+sourav0299@users.noreply.github.com>
2024-10-16 06:53:10 +0530

    Fix: db config error fixed

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,6 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { createOrUpdateUser } from '@/utils/db/action'
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint ,,.
--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -4,6 +4,6 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.NEXT_PUBLIC_NEONDB_URL);
 
 export const db = drizzle(sql, { schema });
